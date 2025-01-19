7dadf9e812dfdeb8f6999ad30086c3b402496160
Abhinav Duvvuri <dvabhinav31@gmail.com>
2025-01-18 18:59:52 -0800

    azure pipline changes

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template, request, Response, stream_with_context
 from openai import OpenAI
 import chromadb
 import json
+import os
+from sympy import EX
+
 
 app = Flask(__name__)
 
 # Initialize ChromaDB and OpenAI
 chroma_client = chromadb.Client()
 collection = chroma_client.create_collection(name="resume_data", get_or_create=True)
-openai_client = OpenAI(api_key='jkj')
+openai_client = OpenAI(api_key=os.environ.get('openai_key'))
 
 # Read and add documents to ChromaDB
 import os
@@ -122,4 +125,7 @@ def ask():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    try:
+        app.run(debug=True) 
+    except:
+        print("Error occured")
